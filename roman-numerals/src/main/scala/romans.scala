56ab453fb7e6d22035340bda6b82aa7fa1e954a3
Pedro Ribeiro <pedroribeirojr@gmail.com>
2016-06-04 21:14:30 -0400

    simple cases VXDLCM

--- roman-numerals/src/main/scala/romans.scala
+++ roman-numerals/src/main/scala/romans.scala
@@ -2,5 +2,16 @@
 package romans
 
 class RomanConverter {
-	def toInt(s: String):Int = 1
+	def toInt(s: String):Int =  { 
+		s match {
+			case "I" => 1
+			case "V" => 5
+			case "X" => 10
+			case "L" => 50
+			case "C" => 100
+			case "D" => 500
+			case "M" => 1000
+			case _   => 0
+		}
+	}
 }
--- roman-numerals/src/test/scala/romans-test.scala
+++ roman-numerals/src/test/scala/romans-test.scala
@@ -6,5 +6,10 @@ class RomansSpec extends FlatSpec with Matchers {
       "A Roman Converter " should " change I to 1 " in {
          converter.toInt("I") should be (1)
       }
-      
+	  "" should "change 'V' to  5" in { converter.toInt("V") should be (5)} 
+	  "" should "change 'X' to  10" in { converter.toInt("X") should be (10)} 
+	  "" should "change 'L' to  50" in { converter.toInt("L") should be (50)} 
+	  "" should "change 'C' to  100 " in  { converter.toInt("C") should be (100)}
+	  "" should "change 'D' to  500 " in  { converter.toInt("D") should be (500)}
+	  "" should "change 'M' to  1000 " in  { converter.toInt("M") should be (1000)}
 }
