80062af0c399d1b2a4b290d67509e42459cf131b
Pedro Ribeiro <pedroribeirojr@gmail.com>
2016-06-04 21:32:22 -0400

    II to 2 with a simple and bad recursion

--- roman-numerals/src/main/scala/romans.scala
+++ roman-numerals/src/main/scala/romans.scala
@@ -5,18 +5,18 @@ class RomanConverter {
 	def toInt(s: String):Int =  {
 		s match {
 			case "" => 0
-			case _ => convertToken(s)
+			case _ => convertToken(s.head) + toInt(s.tail)
 		}
 	}
-	def convertToken(s:String): Int = 
+	def convertToken(s:Char): Int = 
 		s match {
-			case "I" => 1
-			case "V" => 5
-			case "X" => 10
-			case "L" => 50
-			case "C" => 100
-			case "D" => 500
-			case "M" => 1000
+			case 'I' => 1
+			case 'V' => 5
+			case 'X' => 10
+			case 'L' => 50
+			case 'C' => 100
+			case 'D' => 500
+			case 'M' => 1000
 		}
 	
 }
