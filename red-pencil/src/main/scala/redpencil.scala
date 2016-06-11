191614e72d059169aa7baf30fe9193e98eae389d
Pedro Ribeiro <pedroribeirojr@gmail.com>
2016-06-11 10:55:22 -0400

    should exists only if previous price has been stable for at least 30 days

--- red-pencil/src/main/scala/redpencil.scala
+++ red-pencil/src/main/scala/redpencil.scala
@@ -1,16 +1,24 @@
 package redpencil
-import com.github.nscala_time.time.Imports._;
+import com.github.nscala_time.time.Imports._
+
 
 class RedPencilService {
 	def evaluate(p:Product):RedPencilPromotion={
 		if (p == null) throw new IllegalArgumentException("Product Required")
 		if (p.prices == Nil) throw new IllegalArgumentException("Price Required")
-
+		val prices = p.prices.sortWith( _.start > _.start )
+		println(prices)
 		def priceChanged(currentPrice:Double, previousPrice:Double) = currentPrice <= previousPrice * 0.95 && currentPrice >= previousPrice * 0.70
-		def comparePrices(price:Price, previousPrice:Price):Boolean = priceChanged(price.price, previousPrice.price) 
-
-		return p.prices.tail.headOption match {
-			case Some(lastChanged) => RedPencilPromotion(comparePrices(p.prices.head, lastChanged))
+		def debug(label:String, result:Boolean):Boolean = {
+			println(label, result)
+			return result
+		}
+		def comparePrices(price:Price, previousPrice:Price):Boolean = debug ("priceChanged", priceChanged(price.price, previousPrice.price))
+		def wasStableForLast30Days(currentPrice:Price, lastPrice:Price)= debug ( "stable" + lastPrice + currentPrice, currentPrice.start.isAfter(lastPrice.start + 29.days))
+		return prices.tail.headOption match {
+			case Some(lastChanged) => RedPencilPromotion(
+				comparePrices(prices.head, lastChanged) &&
+				wasStableForLast30Days(prices.head, lastChanged))
 			case None => RedPencilPromotion(false)
 		}
 		
