16850313048ae4bad4211b6d79955bb57656eb9b
Pedro Ribeiro <pedroribeirojr@gmail.com>
2016-06-14 07:41:15 -0400

    Fun Spec and refactoring

--- red-pencil/src/main/scala/redpencil.scala
+++ red-pencil/src/main/scala/redpencil.scala
@@ -6,20 +6,29 @@ class RedPencilService {
 	def evaluate(p:Product):RedPencilPromotion={
 		if (p == null) throw new IllegalArgumentException("Product Required")
 		if (p.prices == Nil) throw new IllegalArgumentException("Price Required")
-		val prices = p.prices.sortWith( _.start > _.start )
-		println(prices)
+
+
 		def priceChanged(currentPrice:Double, previousPrice:Double) = currentPrice <= previousPrice * 0.95 && currentPrice >= previousPrice * 0.70
 		def debug(label:String, result:Boolean):Boolean = {
-			println(label, result)
+			//println(label, result)
 			return result
 		}
 		def comparePrices(price:Price, previousPrice:Price):Boolean = debug ("priceChanged", priceChanged(price.price, previousPrice.price))
 		def wasStableForLast30Days(currentPrice:Price, lastPrice:Price)= debug ( "stable" + lastPrice + currentPrice, currentPrice.start.isAfter(lastPrice.start + 29.days))
+		def lastDate(lastPriceChange:Price): LocalDate = { 
+			println("lastPrice", lastPriceChange)
+			return lastPriceChange.start + 30.days
+		}
+
+		val prices = p.prices.sortWith( _.start > _.start )
+
 		return prices.tail.headOption match {
-			case Some(lastChanged) => RedPencilPromotion(
-				comparePrices(prices.head, lastChanged) &&
-				wasStableForLast30Days(prices.head, lastChanged))
-			case None => RedPencilPromotion(false)
+			case Some(previousPriceChange) if (
+				comparePrices(prices.head, previousPriceChange) &&
+				wasStableForLast30Days(prices.head, previousPriceChange) ) => RedPencilPromotion(
+				true,
+				expiration=lastDate(prices.head) )
+			case _ => null
 		}
 		
 
--- red-pencil/src/test/scala/redpencil-test.scala
+++ red-pencil/src/test/scala/redpencil-test.scala
@@ -1,69 +1,79 @@
 import org.scalatest._
 import redpencil._;
 import com.github.nscala_time.time.Imports._;
-class RedPencilSpec extends FlatSpec with Matchers {
-      // Your tests here
+class RedPencilSpec extends FunSpec with Matchers {
+	// Your tests here
 	val service = new RedPencilService()
 	val oldPrice = Price(100.00, LocalDate.now - 3.months)
 
-	behavior of "A red pencil promotion "
-	it should "expect a product" in {
-		intercept[IllegalArgumentException] {
-			service.evaluate(null)
+	describe ( "Given red pencil service it should... " ) {
+		it ("expect a product" ){
+			intercept[IllegalArgumentException] {
+				service.evaluate(null)
+			}
 		}
+		it ("expect a product with a price" ){
+			intercept[IllegalArgumentException] {
+				service.evaluate(Product())
+			}
+		} 
 	}
-	it should "expect a product with a price" in {
-		intercept[IllegalArgumentException] {
-			service.evaluate(Product())
+	describe ( "Given red pencil service would return and Red Pencil Promotion that... " ) {
+		it ("has not started when there were no price changes " ){
+			//println ( "not has started when there are no price changes " )
+			val product = Product(Price(3.00))
+			service.evaluate(product) should be (null)
 		}
-	} 
-	it should "not start when there are no price changes " in {
-		//println ( "not start when there are no price changes " )
-		val expected = RedPencilPromotion(false)
-		val product = Product(Price(3.00))
-		service.evaluate(product) should be (expected)
-	}
-	it should "not start when price drop is less than 5%" in {
-		//println("\n not start when price drop is less than 5%")
-		val expected = RedPencilPromotion(false)
-		val product = Product(Price(98.00), oldPrice)
-		service.evaluate(product) should be (expected)
-	}
-	it should "start when there is a price drop of 5%" in {
-		println("\n start when there is a price drop of 5%")
-		val expected = RedPencilPromotion(true)
-		val product = Product(Price(95.00), oldPrice)
-		service.evaluate(product) should be (expected)
-	}
-	it should "start when there is a price drop of more 5%" in {
-		println("\n start when there is a price drop of more 5%")
-		val expected = RedPencilPromotion(true)
-		val product  = Product(Price(94.00), oldPrice)
-		service.evaluate(product) should be (expected)
-	}
-	it should "but no more than 30%" in {
-		println("\n but no more than 30%")
-		val expected = RedPencilPromotion(false)
-		val product = Product(Price(69.00), oldPrice)
-		service.evaluate(product) should be (expected)
-	}
-	it should "exists only if previous price has been stable for at least 30 days" in {
-		println("\n exists only if previous price has been stable for at least 30 days")
-		val expected = RedPencilPromotion(true)
-		var product = Product(Price(80), Price(100.00, LocalDate.now - 30.days))
-		service.evaluate(product) should be (expected)
-	}
-	it should "not exist if previous price wasn't stable in the last  30 days" in {
-		println("\n not exist if previous price wasn't stable in the last  30 days")
-		val expected = RedPencilPromotion(false)
-		var product = Product(Price(80.00), Price(100.00, LocalDate.now - 10.days))
-		service.evaluate(product) should be (expected)
-	}
-	it should "have an expiration date" in {
-		println("\n have an expiration date")
-		val expected = RedPencilPromotion(true, LocalDate.now + 30.days)
-		val product = Product(Price(80.00), oldPrice)
-		service.evaluate(product) should be (expected)
+		it ("has not started when price drop was less than 5%" ){
+			// println("\n not has started when price drop is less than 5%")
+			val product = Product(Price(98.00), oldPrice)
+			service.evaluate(product) should be (null)
+		}
+		it ("has started when there was a price drop of 5%" ){
+		// println("\n has started when there was a price drop of 5%")
+			val expected = RedPencilPromotion(true)
+			val product = Product(Price(95.00), oldPrice)
+			service.evaluate(product) should be (expected)
+		}
+		it ("has started when there was a price drop of more 5%" ){
+			// println("\n has started when there was a price drop of more 5%")
+			val expected = RedPencilPromotion(true)
+			val product = Product(Price(94.00), oldPrice)
+			service.evaluate(product) should be (expected)
+		}
+		it ("but no more than 30%" ){
+			// println("\n but no more than 30%")
+			val product = Product(Price(69.00), oldPrice)
+			service.evaluate(product) should be (null)
+		}
+		it ("exists only if previous price has been stable for at least 30 days" ){
+			// println("\n exists only if previous price has been stable for at least 30 days")
+			val expected = RedPencilPromotion(true)
+			var product = Product(Price(80), Price(100.00, LocalDate.now - 30.days))
+			service.evaluate(product) should be (expected)
+		}
+		it ("not exist if previous price wasn't stable in the last 30 days" ){
+			// println("\n not exist if previous price wasn't stable in the last 30 days")
+			var product = Product(Price(80.00), Price(100.00, LocalDate.now - 10.days))
+			service.evaluate(product) should be (null)
+		}
+		it ("have an expiration date" ){
+			// println("\n have an expiration date")
+			val expected = RedPencilPromotion(true, LocalDate.now + 30.days)
+			val product = Product(Price(80.00), oldPrice)
+			service.evaluate(product) should be (expected)
+		}
+		it ("have an expiration day of 30 days ..." ){
+			val expected = RedPencilPromotion(true, LocalDate.now + 30.days)
+			val product = Product(Price(80.00), oldPrice)
+			service.evaluate(product) should be (expected)		
+		}
+		it ("have an expiration day of 30 days from the day of the last price change " ){
+			println ( "... from the day of the last price change " )
+			val expected = RedPencilPromotion(true, LocalDate.now - 10.days)
+			val product = Product(Price(80.00, start=LocalDate.now - 40.days), oldPrice)
+			service.evaluate(product) should be (expected)		
+		}
 	}
 
 
